43eede0447721c258968c6e7e128dcf679840f75
freedomray3310 <sdetim@gmail.com>
2019-12-10 21:21:56 +0800

    ledctrl

---
+++ check1.py
@@ -0,0 +1,24 @@
+[
+    {
+        "id": "8b09e875.8cba88",
+        "type": "http request",
+        "z": "16e38af3.fadfe5",
+        "name": "",
+        "method": "GET",
+        "ret": "txt",
+        "paytoqs": false,
+        "url": "http://api.mediatek.com/mcs/v2/devices/DgIXVwn5/datachannels/LEDCTRL/datapoints.csv",
+        "tls": "",
+        "proxy": "",
+        "authType": "",
+        "x": 450,
+        "y": 420,
+        "wires": [
+            [
+                "8aac085c.262fb8",
+                "e01fb022.762a7"
+            ]
+        ]
+    }
+]
+
